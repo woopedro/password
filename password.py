011f430f39d9c5c99f99193182acb3612c9cd8b1
woopedro <pasanghaya@gmail.com>
2020-03-29 16:43:40 +0800

    password

--- password.py
+++ password.py
@@ -1,16 +1,13 @@
-x = 1 
-y = 2
+y = 3
 print('最多輸入3次密碼')
-while x <= 3 and y >= 0:
+while y > 0:
+    y = y - 1
     password = input('請輸入密碼:')
     if password == 'a123456':
-    	print('登入成功')
-    	break
-    elif password != 'a123456':
-    	print('密碼錯誤', '還有', y , '次機會')
-    	x = x + 1
-    	y = y - 1
-while x >= 3 and y <= 0:
-    	print('登入失敗')
-    	break
-    
+        print('登入成功')
+        break
+    else:
+        if y > 0:
+            print('密碼錯誤', '還有', y , '次機會')
+        else:
+            print('沒機會嘗試，鎖帳號了！')
